02d8b9da5e26fbe617c8ef9b958b643c84732e2d
Hassan Ali <helfi92@gmail.com>
2018-08-16 09:06:49 -0400

    Put declaration file in src
    Anything in `src` is linted so we should put it there

---
+++ src/index.d.ts
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { ExpansionPanelSummaryProps } from '@material-ui/core/ExpansionPanelSummary';
+import { ExpansionPanelDetailsProps } from '@material-ui/core/ExpansionPanelDetails';
+import { ListItemProps } from '@material-ui/core/ListItem';
+
+export interface Tree {
+    value: string;
+    nodes?: Array<string | Tree>;
+}
+
+export interface MuiTreeViewProps {
+    /**
+     * The data to render as a tree view
+     */
+    tree: Tree[];
+
+    /**
+     * Callback function fired when a tree leaf is clicked.
+     */
+    onLeafClick?: (value: string, parent: Tree) => void;
+
+    /**
+     * A search term to refine the tree
+     */
+    searchTerm?: string;
+
+    /**
+     * Properties applied to the ExpansionPanelSummary element.
+     */
+    expansionPanelSummaryProps?: ExpansionPanelSummaryProps;
+
+    /**
+     * Properties applied to the ExpansionPanelDetails element.
+     */
+    expansionPanelDetailsProps?: ExpansionPanelDetailsProps;
+
+    /**
+     * Properties applied to the ListItem element.
+     */
+    listItemProps?: ListItemProps;
+}
+
+export default class MuiTreeView extends React.Component<MuiTreeViewProps> {}
