014f0ce2ddc13aed06162d9f1e62134c390eb7c7
Hassan Ali <helfi92@gmail.com>
2019-10-21 19:45:52 -0400

    Add scripts directory

---
+++ scripts/copy-files.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cp {src/index.d.ts,package.json,LICENSE,README.md} build
---
+++ scripts/test-release.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+npm config set registry http://localhost:4873/;
+
+# Add npm user so we can use it to publish
+scripts/npm-adduser.js;
+
+# Delete its corresponding verdaccio storage so that we don't have to change the version in order to publish
+rm -rf $HOME/.local/share/verdaccio/storage/material-ui-treeview/
+
+npm publish build --registry http://localhost:4873/
+
+npm config set registry https://registry.npmjs.org/;
